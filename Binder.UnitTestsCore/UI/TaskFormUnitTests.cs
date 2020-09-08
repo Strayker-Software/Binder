68abe670fde0f3e889f51fd0639b2cd101497209
Strayker <45916090+StraykerPL@users.noreply.github.com>
2020-09-08 09:25:35 +0200

    TaskForm and TextMessageBox managers UI test files added

---
+++ Binder.UnitTestsCore/UI/TaskFormUnitTests.cs
@@ -0,0 +1,77 @@
+﻿#define TEST
+using System;
+using System.Windows.Forms;
+using Binder.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/* For copying to new methods:
+ // Prepare:
+
+ // Execute:
+
+ // Verify:
+ // Prepare and Execute:
+ */
+namespace Binder.UnitTests.UI
+{
+    [TestClass]
+    public class TaskFormUnitTests
+    { // TODO: Finish for TaskFormManager class!
+        [TestMethod]
+        public void TaskFormClass_FormState_FormLoadedProperly()
+        {
+            // Prepare:
+            var tsk = new DataGridViewTask();
+            var frmMgr = new Binder.UI.TaskFormManager(new TaskForm(tsk, false), tsk, false);
+            // Execute:
+            var result = frmMgr.LoadForm();
+            // Verify:
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TaskFormClass_DataManage_InputSavedProperly()
+        {
+            // Prepare:
+            var tskStart = new DataGridViewTask()
+            {
+                Name = "Task",
+                Date = DateTime.Now,
+                IfToday = CheckState.Checked
+            };
+            var frmMgr = new Binder.UI.TaskFormManager(new TaskForm(tskStart, false), tskStart, false);
+            frmMgr.LoadForm();
+            frmMgr.OKButtonPressed();
+            // Execute:
+            //var result = 
+            // Verify:
+            Assert.AreEqual("Test", result);
+        }
+
+        [TestMethod]
+        public void TaskFormClass_DialogResult_DialogReturnedOK()
+        {
+            // Prepare:
+            var tsk = new DataGridViewTask();
+            var frmMgr = new Binder.UI.TaskFormManager(new TaskForm(tsk, false), tsk, false);
+            frmMgr.LoadForm();
+            // Execute:
+            //var result = 
+            // Verify:
+            //Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TaskFormClass_DialogResult_DialogReturnedCancel()
+        {
+            // Prepare:
+            var tsk = new DataGridViewTask();
+            var frmMgr = new Binder.UI.TaskFormManager(new TaskForm(tsk, false), tsk, false);
+            frmMgr.LoadForm();
+            // Execute:
+            //var result = 
+            // Verify:
+            //Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Binder.UnitTestsCore/UI/TextMessageBoxUnitTests.cs
@@ -0,0 +1,68 @@
+﻿#define TEST
+using System;
+using System.Windows.Forms;
+using Binder.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/* For copying to new methods:
+ // Prepare:
+
+ // Execute:
+
+ // Verify:
+ // Prepare and Execute:
+ */
+namespace Binder.UnitTests.UI
+{
+    [TestClass]
+    public class TextMessageBoxUnitTests
+    { // TODO: Finish for TextMessageBoxManager class!
+        [TestMethod]
+        public void TextMessageBoxClass_FormState_FormLoadedProperly()
+        {
+            // Prepare:
+            var frmMgr = new Binder.UI.TextMessageBoxManager(new TextMessageBox(), "This is TextMessageBox");
+            // Execute:
+            var result = frmMgr.LoadForm();
+            // Verify:
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TextMessageBoxClass_DataManage_InputSavedProperly()
+        {
+            // Prepare:
+            var frmMgr = new Binder.UI.TextMessageBoxManager(new TextMessageBox(), "This is TextMessageBox");
+            frmMgr.LoadForm();
+            frmMgr.Form.Input.Text = "Test";
+            // Execute:
+            var result = frmMgr.Form.Input.Text;
+            // Verify:
+            Assert.AreEqual("Test", result);
+        }
+
+        [TestMethod]
+        public void TextMessageBoxClass_DialogResult_DialogReturnedOK()
+        {
+            // Prepare:
+            var frmMgr = new Binder.UI.TextMessageBoxManager(new TextMessageBox(), "This is TextMessageBox");
+            frmMgr.LoadForm();
+            // Execute:
+            //var result = 
+            // Verify:
+            //Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TextMessageBoxClass_DialogResult_DialogReturnedCancel()
+        {
+            // Prepare:
+            var frmMgr = new Binder.UI.TextMessageBoxManager(new TextMessageBox(), "This is TextMessageBox");
+            frmMgr.LoadForm();
+            // Execute:
+            //var result = 
+            // Verify:
+            //Assert.IsTrue(result);
+        }
+    }
+}
