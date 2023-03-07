b8dd483fa01d19f82e789f20f07e9b243ff8247a
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-07 20:45:59 +0100

    feat: added tables listt component

---
+++ binder-web-frontend/src/pages/home/components/tables-list/tables-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-tables-list',
+  templateUrl: './tables-list.component.html',
+  styleUrls: ['./tables-list.component.scss'],
+})
+export class TablesListComponent {
+  panelOpenState = false;
+}
+
+
