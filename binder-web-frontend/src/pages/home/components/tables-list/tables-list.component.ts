8af4ac0530215b4124ac45218018dc4634648c5f
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-11 09:53:15 +0100

    refactor: refactore-after-review

--- binder-web-frontend/src/pages/home/components/tables-list/tables-list.component.ts
+++ binder-web-frontend/src/pages/home/components/tables-list/tables-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tables-list',
+  selector: 'tables-list',
   templateUrl: './tables-list.component.html',
   styleUrls: ['./tables-list.component.scss'],
 })
 export class TablesListComponent {
-  panelOpenState = false;
+
 }
 
 
