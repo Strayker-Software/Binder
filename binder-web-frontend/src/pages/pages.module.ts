6cd01475c28772433fa0926919ae37dd27a628a4
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-28 17:23:30 +0200

    refactor: refactored after hints from review

--- binder-web-frontend/src/pages/pages.module.ts
+++ binder-web-frontend/src/pages/pages.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './home/components/navbar/navbar.component';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatDividerModule
   ],
-  declarations: [HomeComponent, NavbarComponent],
+  declarations: [
+    HomeComponent,
+    NavbarComponent
+  ],
   exports: [HomeComponent]
 })
 export class PagesModule { }
