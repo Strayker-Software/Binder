993e9d5efe259138e94bbe9c4ef19c9a7ca1c7bd
Strayker <45916090+StraykerPL@users.noreply.github.com>
2023-10-01 23:07:27 +0200

    feat(webfrontend): Add Domain Buttons component

---
+++ binder-web-frontend/src/pages/home/components/domain-buttons/domain-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'domain-buttons',
+  templateUrl: './domain-buttons.component.html',
+  styleUrls: ['./domain-buttons.component.scss']
+})
+export class DomainButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
