17903bb4afafcc25fbee14ad92c9c95edab05714
Strayker <45916090+StraykerPL@users.noreply.github.com>
2023-10-03 23:48:39 +0200

    refactor(webfrontend): Finish handling and styles for Domain Buttons compoent
    Add events to handle clicking,
    Change px values to REM

--- binder-web-frontend/src/pages/home/components/domain-buttons/domain-buttons.component.ts
+++ binder-web-frontend/src/pages/home/components/domain-buttons/domain-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'domain-buttons',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./domain-buttons.component.scss']
 })
 export class DomainButtonsComponent implements OnInit {
+  @Output() optionsClickedEvent: EventEmitter<HTMLParagraphElement> = new EventEmitter();
+  @Output() aboutClickedEvent: EventEmitter<HTMLParagraphElement> = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  optionsClicked() {
+    this.optionsClickedEvent.emit();
   }
 
+  aboutClicked() {
+    this.aboutClickedEvent.emit();
+  }
 }
