c69a3fa7ffef5e10150f7e3fd278ad513b12dd49
Lukash88 <kocyq88@gmail.com>
2023-10-23 23:32:38 +0200

    refactor(webfrontend): Replace mocked version value with returned from Web Backend

--- binder-web-frontend/src/pages/home/components/header/header.component.ts
+++ binder-web-frontend/src/pages/home/components/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subject, take } from 'rxjs';
+import { AppVersion, AppVersionsService } from "src/api";
+import { DisplayableVersion } from "src/shared/models/displayableVersion";
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { 
-   version: number = 0;
+export class HeaderComponent implements OnInit, OnDestroy { 
+  private subscribe$: Subject<void> = new Subject<void>();
+  attributes: string[] = ['major', 'minor', 'patch', 'name'];
+  version?: DisplayableVersion;
+
+  constructor(private appVersionsService: AppVersionsService) {}
+  
+  ngOnInit(): void {
+    this.appVersionsService
+    .getVersionGet(1)
+    .pipe(take(1))
+      .subscribe({
+        next: (version: AppVersion) => {
+          const displayableVerion: DisplayableVersion = {
+            name: version.name,
+            major: version.major,
+            minor: version.minor,
+            patch: version.patch
+          } as DisplayableVersion;
+
+          this.version = displayableVerion;
+        },
+        error: (error: any) => {
+          console.error(error);
+        },
+      });
+  } 
+  
+  ngOnDestroy(): void {
+    this.subscribe$.unsubscribe();
+  }
 }
