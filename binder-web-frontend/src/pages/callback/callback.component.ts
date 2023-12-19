1b843b9ad1f2514fd3d38e630953067186594d4a
Strayker <45916090+StraykerPL@users.noreply.github.com>
2023-12-19 22:49:26 +0100

    feat(webfrontend): Add auth cookie creation on client side

--- binder-web-frontend/src/pages/callback/callback.component.ts
+++ binder-web-frontend/src/pages/callback/callback.component.ts
@@ -11,7 +11,10 @@ export class CallbackComponent {
   constructor(private authService: AuthService, private route: ActivatedRoute) {
     this.authService
       .apiAuthCallbackPost(this.route.snapshot.paramMap.get('code')?.toString());
-    
+      const date = new Date();
+      date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+      document.cookie = "binder_token=" + "token" + "; expires=" + date.toUTCString() + "; path=/; HttpOnly; Secure";
+
       window.location.replace("/home");
   }
 }
